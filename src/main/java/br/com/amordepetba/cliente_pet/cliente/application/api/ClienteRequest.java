ad45e613698b6fbfb6915a6078de6c5fa5c0ef59
paulo <pbrsj1385@gmail.com>
2024-11-22 10:39:17 -0300

    implementa request e response

--- src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.amordepetba.cliente_pet.cliente.application.api;
 
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.amordepetba.cliente_pet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
+	
 
 }
