add276d7dbccb2695e7699d2bc4809e905e4f17d
paulo <pbrsj1385@gmail.com>
2024-11-29 10:36:10 -0300

    implemanta fluxo repository, tete ok!

--- src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -1,12 +1,18 @@
 package br.com.amordepetba.cliente_pet.cliente.application.api;
 
+import java.util.List;
 import java.util.UUID;
+
+import br.com.amordepetba.cliente_pet.cliente.domain.Cliente;
 
 public class ClienteListResponse {
 
 	private UUID idCliente;
 	private String nomeCompleto;
-	private String cpf;
 	private String email;
 	private String celular;
+	
+	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+		return null;
+	}
 }
