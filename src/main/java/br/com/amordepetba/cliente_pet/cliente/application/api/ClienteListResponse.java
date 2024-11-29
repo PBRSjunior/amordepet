a017f282bd86c31ea4319e26f4e65e0dc6e906ca
paulo <pbrsj1385@gmail.com>
2024-11-29 11:14:27 -0300

    implementa getTodos

--- src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/amordepetba/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -2,17 +2,32 @@ package br.com.amordepetba.cliente_pet.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.amordepetba.cliente_pet.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
-
 	private UUID idCliente;
 	private String nomeCompleto;
+	private String cpf;
 	private String email;
 	private String celular;
-	
+
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
+
+	private ClienteListResponse(Cliente cliente) {
+		super();
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+	}
+
 }
